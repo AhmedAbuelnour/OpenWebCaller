dfb548ddd596cfd126d1bcbdc2ac746d498557f4
Ahmed Ramadan <A7medRamadan@outlook.com>
2017-08-23 14:32:32 +0200

    Version 1.2
    Providing more Flexibility to handle the response.

--- WebCaller.cs
+++ WebCaller.cs
@@ -7,6 +7,24 @@
     using System.Threading.Tasks;
 
     /// <summary>
+    /// Class Represents the Respond Result
+    /// </summary>
+    public class HttpRespondResult
+    {
+        /// <summary>
+        /// The Server returned Content
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// The HttpRespond Code "Ok" , "Not Found" , "Unauthorized" , etc.
+        /// </summary>
+        public string StatusCode { get; set; }
+        /// <summary>
+        /// Indicate if the request has an issue to be completed as expected 
+        /// </summary>
+        public bool IsSuccess { get; set; }
+    }
+    /// <summary>
     /// Encapsulates All HTTP Requests 
     /// </summary>
     public static class WebCaller
@@ -17,11 +35,17 @@
         /// </summary>
         /// <param name="Url"> Url of the remote server, Make sure to format the Url in case of sending a value along with Url </param>
         /// <returns>The Respond of the remote server in format of Json</returns>           
-        public static async Task<string> GetAsync(string Url)
+        public static async Task<HttpRespondResult> GetAsync(string Url)
         {
             using (HttpClient Client = new HttpClient())
             {
-                return await (await Client.GetAsync(Url)).Content.ReadAsStringAsync();
+                HttpResponseMessage httpResponseMessage = await Client.GetAsync(Url);
+                return new HttpRespondResult
+                {
+                    Content = await httpResponseMessage.Content.ReadAsStringAsync(),
+                    StatusCode = httpResponseMessage.StatusCode.ToString(),
+                    IsSuccess = httpResponseMessage.IsSuccessStatusCode
+                };
             }
         }
         /// <summary>
@@ -31,14 +55,20 @@
         /// <param name="Url">Url of the remote server</param>
         /// <param name="Content">The Data you want to send to the server, it will be formatted automatically into Json</param>
         /// <returns>The Respond of the remote server in format of Json, OK, Not Found, Created, etc.</returns>
-        public static async Task<string> PostAsync<T>(string Url, T Content)
+        public static async Task<HttpRespondResult> PostAsync<T>(string Url, T Content)
         {
             using (HttpClient Client = new HttpClient())
             {
                 using (ByteArrayContent ByteContent = new ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Content))))
                 {
                     ByteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                    return (await Client.PostAsync(Url, ByteContent)).ReasonPhrase;
+                    HttpResponseMessage httpResponseMessage = await Client.PostAsync(Url, ByteContent);
+                    return new HttpRespondResult
+                    {
+                        Content = await httpResponseMessage.Content.ReadAsStringAsync(),
+                        StatusCode = httpResponseMessage.StatusCode.ToString(),
+                        IsSuccess = httpResponseMessage.IsSuccessStatusCode
+                    };
                 }
             }
         }
@@ -49,14 +79,20 @@
         /// <param name="Url">Url of the remote server</param>
         /// <param name="Content">The Data you want to update to the server, it will be formatted automatically into Json, Make sure to format the Url in case of sending a value along with Url</param>
         /// <returns>The Respond of the remote server in format of Json, OK, Not Found, Created, etc.</returns>
-        public static async Task<string> PutAsync<T>(string Url, T Content)
+        public static async Task<HttpRespondResult> PutAsync<T>(string Url, T Content)
         {
             using (HttpClient Client = new HttpClient())
             {
                 using (ByteArrayContent ByteContent = new ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Content))))
                 {
                     ByteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                    return (await Client.PutAsync(Url, ByteContent)).ReasonPhrase;
+                    HttpResponseMessage httpResponseMessage = await Client.PutAsync(Url, ByteContent);
+                    return new HttpRespondResult
+                    {
+                        Content = await httpResponseMessage.Content.ReadAsStringAsync(),
+                        StatusCode = httpResponseMessage.StatusCode.ToString(),
+                        IsSuccess = httpResponseMessage.IsSuccessStatusCode
+                    };
                 }
             }
         }
@@ -65,11 +101,17 @@
         /// </summary>
         /// <param name="Url"> Url of the remote server, Make sure to format the Url in case of sending a value along with Url </param>
         /// <returns>The Respond of the remote server in format of Json, OK, Not Found, Created, etc.</returns>
-        public static async Task<string> DeleteAsync(string Url)
+        public static async Task<HttpRespondResult> DeleteAsync(string Url)
         {
             using (HttpClient Client = new HttpClient())
             {
-                return (await Client.DeleteAsync(Url)).ReasonPhrase;
+                HttpResponseMessage httpResponseMessage = await Client.DeleteAsync(Url);
+                return new HttpRespondResult
+                {
+                    Content = await httpResponseMessage.Content.ReadAsStringAsync(),
+                    StatusCode = httpResponseMessage.StatusCode.ToString(),
+                    IsSuccess = httpResponseMessage.IsSuccessStatusCode
+                };
             }
         }
     }
